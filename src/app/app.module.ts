d4959ea28e1d0c454f85c2d039d6a5267c6fe470
Ezequiel Lopez <skiel.j.lopez@gmail.com>
2017-11-11 02:12:05 -0500

    home page almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-
+import { MatToolbarModule } from '@angular/material';
+import { RoutesModule } from './routes/routes.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    RoutesModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
